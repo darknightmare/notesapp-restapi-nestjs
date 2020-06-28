b3b2fe38703d2aa9a928d4ca5d86614e5a7e5aec
darknightmare <rubentkd98@gmail.com>
2020-06-28 22:58:03 +0000

    Apply the initial NestJS users authentication techniques, the local strategy and some cleaning

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { ApiModule } from './api/api.module';
 import { MongoModule } from './database/mongo.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [ApiModule, MongoModule],
+  imports: [ApiModule, MongoModule, AuthModule],
   controllers: [AppController],
-  providers: [AppService],
 })
 export class AppModule {}
