89b7971ef41541865dad6445513cb1ca0c7ebbbd
darknightmare <rubentkd98@gmail.com>
2020-06-28 23:02:54 +0000

    Move to own local auth guard

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,10 +1,10 @@
 import { Controller, UseGuards, Post, Request } from '@nestjs/common';
-import { AuthGuard } from '@nestjs/passport';
+import { LocalAuthGuard } from './auth/local-auth.guard';
 
 
 @Controller()
 export class AppController {
-  @UseGuards(AuthGuard('local'))
+  @UseGuards(LocalAuthGuard)
   @Post('users/login')
   async login(@Request() req) {
     return req.user;
